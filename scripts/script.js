019ec4a251dc3fcbc49767311ae41fbef2ac4124
PaulBarkhov <63750949+PaulBarkhov@users.noreply.github.com>
2021-01-09 20:12:27 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -36,7 +36,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
     //ADD TASK
-    const add__task__button = document.querySelector(".add__task__button"),
+    const toDo = document.querySelector('#toDo'),
+        
+          add__task__button = document.querySelector(".add__task__button"),
           content = document.querySelector(".content"),
           add__task__window__wrapper = document.querySelector(".add__task__window__wrapper"),
           
@@ -70,125 +72,225 @@ document.addEventListener("DOMContentLoaded", () => {
         notification_1hour = false,
         notification_1day = false;
 
-        add__task__button.addEventListener("click", (event) => {
-            event.preventDefault();
+    add__task__button.addEventListener("click", () => {toggleAddTask()} );
 
-            add__task__button.classList.toggle("rotate");
+    function toggleAddTask() {
+        event.preventDefault();
 
-            add__task__window__wrapper.classList.toggle("hide");
-            content.classList.toggle("hide"); 
-            
-            if (set__task__name.classList.contains("hide")) {
-                set__task__name.classList.toggle("hide");
+        add__task__button.classList.toggle("rotate");
+
+        add__task__window__wrapper.classList.toggle("hide");
+        content.classList.toggle("hide"); 
+        
+        if (set__task__name.classList.contains("hide")) {
+            set__task__name.classList.toggle("hide");
+            toDo.innerHTML = "New task";
+        }
+        else {
+            toDo.innerHTML = "To Do List";
+            set__task__name.classList.toggle("hide");
+        }
+    }
+
+    task__name__OK__button.addEventListener("click", (event) => {
+        event.preventDefault();
+
+        name = task__name__input.value;
+
+        set__task__name.classList.toggle("hide");
+        set__task__color.classList.toggle("hide");
+        task__name__input.value = "New Task";
+    });
+
+    task__colors.forEach(item => {
+        item.addEventListener("click", (event) => {
+            color = event.target.style.backgroundColor;
+
+            set__task__color.classList.toggle("hide");
+            if (date == "") {
+                set__task__date.classList.toggle("hide");
+            }
+            else {
+                set__task__time.classList.toggle("hide");
             }
         });
+    });
 
-        task__name__OK__button.addEventListener("click", (event) => {
-            event.preventDefault();
+    task__date__OK__button.addEventListener("click", (event) => {
+        event.preventDefault();
 
-            name = task__name__input.value;
+        set__task__date.classList.toggle("hide");
+        set__task__time.classList.toggle("hide");
+    })
 
-            set__task__name.classList.toggle("hide");
-            set__task__color.classList.toggle("hide");
-            task__name__input.value = "New Task";
-        });
+    task__time__OK__button.addEventListener("click", (event) => {
+        event.preventDefault();
 
-        task__colors.forEach(item => {
-            item.addEventListener("click", (event) => {
-                color = event.target.style.backgroundColor;
+        time = task__time__input.value;
 
-                set__task__color.classList.toggle("hide");
-                set__task__date.classList.toggle("hide");
+        set__task__time.classList.toggle("hide");
+        set__task__notifications.classList.toggle("hide");
+        task__time__input.value = "";
+    });
 
-                let currentDate = new Date(),
-                dd = currentDate.getDate(),
-                mm = currentDate.getMonth() + 1,
-                yy = currentDate.getFullYear();
-        
-                if (dd < 10) dd = '0' + dd;
-                if (mm < 10) mm = '0' + mm;
-        
-                let today = `${yy}-${mm}-${dd}`;
-    
-                task__date__input.value = today;
-            });
-        });
+    task__notifications__OK__button.addEventListener("click", (event) => {
+        event.preventDefault();
 
-        task__date__OK__button.addEventListener("click", (event) => {
-            event.preventDefault();
+        toDo.innerHTML = "To Do List";
 
-            date = task__date__input.value;
-            date = date[8] + date[9] + '.' + date[5] + date[6];
+        notification_10min = notifications[0].checked;
+        notification_1hour = notifications[1].checked;
+        notification_1day = notifications[2].checked;
 
-            set__task__date.classList.toggle("hide");
-            set__task__time.classList.toggle("hide");
-        })
+        set__task__notifications.classList.toggle("hide");
+        set__task__ready.classList.toggle("hide");
 
-        task__time__OK__button.addEventListener("click", (event) => {
-            event.preventDefault();
+        //task = new Task(name, color, date, time, notification_10min, notification_1hour, notification_1day);
 
-            time = task__time__input.value;
-
-            set__task__time.classList.toggle("hide");
-            set__task__notifications.classList.toggle("hide");
-            task__time__input.value = "";
-        });
-
-        task__notifications__OK__button.addEventListener("click", (event) => {
-            event.preventDefault();
-
-            notification_10min = notifications[0].checked;
-            notification_1hour = notifications[1].checked;
-            notification_1day = notifications[2].checked;
-
-            set__task__notifications.classList.toggle("hide");
-            set__task__ready.classList.toggle("hide");
-
-            //task = new Task(name, color, date, time, notification_10min, notification_1hour, notification_1day);
-
-            task = document.createElement("div");
-            task.innerHTML = `
-                <hr>
-                <div class="task">
-                    <div style="background-color: ${color}" class="task__color">
-                        <a href=""></a>
-                    </div>
-
-                    <div class="task__name">
-                        <p class="task__text">${name}</p>
-                    </div>
-
-                    <div class="task__date">
-                        <p>${date}</p>
-                    </div>
-                    
-                    <div class="task__time">
-                        <p>${time}</p>
-                    </div>
+        task = document.createElement("div");
+        task.innerHTML = `
+            <div class="task">
+                <div style="background-color: ${color}" class="task__color">
+                    <a href=""></a>
                 </div>
-                <hr>
-            `
-            content.append(task);
 
-            set__task__ready.classList.toggle("hide");
-            content.classList.toggle("hide");
-            add__task__window__wrapper.classList.toggle("hide");
-            add__task__button.classList.toggle("rotate");
+                <div class="task__name">
+                    <p class="task__text">${name}</p>
+                </div>
 
-            // setTimeout(() => {
-            //     set__task__ready.classList.toggle("hide");
-            //     content.classList.toggle("hide");
-            // }, 5000);
+                <div class="task__date">
+                    <p>${date}</p>
+                </div>
+                
+                <div class="task__time">
+                    <p>${time}</p>
+                </div>
+            </div>
+        `
+        content.append(task);
 
-        });
+        date = "";
+        set__task__ready.classList.toggle("hide");
+        content.classList.toggle("hide");
+        add__task__window__wrapper.classList.toggle("hide");
+        add__task__button.classList.toggle("rotate");
 
-        //DELETE TASK
-        // const delete__task__button = document.querySelectorAll(".delete__task__button");
+        // setTimeout(() => {
+        //     set__task__ready.classList.toggle("hide");
+        //     content.classList.toggle("hide");
+        // }, 5000);
 
-        // delete__task__button.forEach(item => {
-        //     item.addEventListener("click", (event) => {
-        //         console.log("123");
-        //     })
-        // });
+    });
+
+    //DELETE TASK
+    // const delete__task__button = document.querySelectorAll(".delete__task__button");
+
+    // delete__task__button.forEach(item => {
+    //     item.addEventListener("click", (event) => {
+    //         console.log("123");
+    //     })
+    // });
+
+
+    //CALENDAR 
+    const calendar__currentMonth = document.querySelector(".calendar__currentMonth"),
+          calendar__previousMonth = document.querySelector(".calendar__previousMonth"),
+          calendar__nextMonth = document.querySelector(".calendar__nextMonth"),
+          calendar__days = document.querySelector(".calendar__days");
+
+          currentDate = new Date();
+          months = ["Januar", "Februar", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
         
+    let days,
+        exportMonth;
+
+    function buildCalendar () {
+        let mm = currentDate.getMonth(),
+            yy = currentDate.getFullYear(),
+
+            currentMonth = months[mm];
+
+        exportMonth = mm+1;
+        if (exportMonth < 10) {
+            exportMonth = "0" + exportMonth;
+        };
+
+        let lastDay = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate(),
+            lastDayIndex = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDay(),
+            firstDayIndex = currentDate;
+                            firstDayIndex.setDate(1); 
+                            firstDayIndex = firstDayIndex.getDay();
+
+        let prevMonthLastDay = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0).getDate();
+
+
+        nextMonth = months[months.indexOf(currentMonth) + 1];
+        previousMonth = months[months.indexOf(currentMonth) - 1];
+
+        if (currentMonth == "Januar") {
+            previousMonth = months[11];
+        }
+        if (currentMonth == "December") {
+            nextMonth = months[0];
+        }
+
+        calendar__currentMonth.innerHTML = `${currentMonth} ${yy}`;
+        calendar__previousMonth.innerHTML = `${previousMonth}`;
+        calendar__nextMonth.innerHTML = `${nextMonth}`;
+
+        days = document.createElement("div");
+        days.classList.add("days");
+        calendar__days.append(days);
+
+        for (i = firstDayIndex - 1; i > 0; i--) {
+            days.innerHTML += `<div class="calendar__anotherMonth">${prevMonthLastDay - i}</div>`;
+        }; // adds previous month`s days to start
+
+        for (i = 1; i <= lastDay; i++) {
+            days.innerHTML += `<div>${i}</div>`;
+        };  // add all days
+
+        for (i = 1; i <= 7 - lastDayIndex; i++) {
+            days.innerHTML += `<div class="calendar__anotherMonth">${i}</div>`;
+        };
+    };
+
+    buildCalendar();
+
+    calendar__nextMonth.addEventListener("click", () => {
+        days.remove();
+
+        let mm = currentDate.getMonth();
+        currentDate.setMonth(mm + 1);
+
+        currentMonth = months[currentDate.getMonth()];
+
+        buildCalendar();
+    });
+
+    calendar__previousMonth.addEventListener("click", () => {
+        days.remove();
+
+        let mm = currentDate.getMonth();
+        currentDate.setMonth(mm - 1);
+
+        currentMonth = months[currentDate.getMonth()];
+
+        buildCalendar();
+    });
+
+    calendar__days.addEventListener("click", (event) => {
+        if (!event.target.classList.contains("days")) {
+            myFunction(event.target.innerHTML);
+        }
+    });
+
+    function myFunction(clickedDay) {
+        if (clickedDay < 10) {
+            clickedDay = "0" + clickedDay;
+        }
+        date = clickedDay + "." + exportMonth;
+        toggleAddTask();
+    };
+                
 });
